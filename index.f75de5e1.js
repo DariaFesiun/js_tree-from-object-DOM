19d9eb96c59c77f98a343dc1a52df6f9eea73faa
daradanilova <lemma1102@gmail.com>
2024-08-29 13:12:21 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,33 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    const ul = document.createElement("ul");
+    for(const key in data){
+        const li = document.createElement("li");
+        ul.appendChild(li);
+        li.textContent = key;
+        if (typeof data[key] === "object") {
+            element.append(ul);
+            createTree(li, data[key]);
+        }
+    }
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
